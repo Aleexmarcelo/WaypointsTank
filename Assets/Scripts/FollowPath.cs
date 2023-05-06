a36b3b6a28ea5ca7d41ff217e8c666c15f363726
Aleexmarcelo <83593099+Aleexmarcelo@users.noreply.github.com>
2023-05-05 21:31:07 -0300

    funcionando sem nav mesh

--- Assets/Scripts/FollowPath.cs
+++ Assets/Scripts/FollowPath.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 public class FollowPath : MonoBehaviour
 {
     Transform goal;
@@ -12,45 +13,51 @@ public class FollowPath : MonoBehaviour
     GameObject currentNode;
     int currentWP = 0;
     Graph g;
+
     void Start()
     {
         wps = wpManager.GetComponent<WPManager>().waypoints;
         g = wpManager.GetComponent<WPManager>().graph;
         currentNode = wps[0];
     }
+
     public void GoToHeli()
     {
         g.AStar(currentNode, wps[1]);
         currentWP = 0;
     }
+
     public void GoToRuin()
     {
         g.AStar(currentNode, wps[6]);
         currentWP = 0;
     }
+
     void LateUpdate()
     {
         if (g.getPathLength() == 0 || currentWP == g.getPathLength())
             return;
-        //O nó que estará mais próximo neste momento
+
         currentNode = g.getPathPoint(currentWP);
-        //se estivermos mais próximo bastante do nó o tanque se moverá para o próximo
         if (Vector3.Distance(
-        g.getPathPoint(currentWP).transform.position,
-        transform.position) < accuracy)
+            g.getPathPoint(currentWP).transform.position,
+            transform.position) < accuracy)
         {
             currentWP++;
         }
+
         if (currentWP < g.getPathLength())
         {
             goal = g.getPathPoint(currentWP).transform;
             Vector3 lookAtGoal = new Vector3(goal.position.x,
-            this.transform.position.y,
-            goal.position.z);
+                this.transform.position.y,
+                goal.position.z);
             Vector3 direction = lookAtGoal - this.transform.position;
             this.transform.rotation = Quaternion.Slerp(this.transform.rotation,
-            Quaternion.LookRotation(direction),
-            Time.deltaTime * rotSpeed);
+                Quaternion.LookRotation(direction),
+                Time.deltaTime * rotSpeed);
+
+            transform.position = Vector3.MoveTowards(transform.position, goal.position, speed * Time.deltaTime);
         }
     }
 }
